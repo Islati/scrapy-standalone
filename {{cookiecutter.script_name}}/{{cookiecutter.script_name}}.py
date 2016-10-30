5ad26b2dd7fe32907a23fd073f50eca6788c6b09
Brandon <brandon@sayfriends.com>
2016-10-30 14:31:08 -0230

    Implemented output_file_type and output_file_name to specify scrapy file output when executing the script

--- {{cookiecutter.script_name}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.script_name}}/{{cookiecutter.script_name}}.py
@@ -26,19 +26,21 @@ class {{cookiecutter.script_name | title}}Spider(Spider):
     def parse(self, response):
         pass
 
-
 process = CrawlerProcess({
     'COOKIES_ENABLED': False,
     'SPIDER_MIDDLEWARES': {
         'scrapy.contrib.spidermiddleware.referer.RefererMiddleware': True
-    }
-    {% if cookiecutter.use_crawlera == true %}
-    ,'DOWNLOADER_MIDDLEWARES': {
+    },
+    {% if cookiecutter.use_crawlera == "true" %}
+    'DOWNLOADER_MIDDLEWARES': {
         'scrapy_crawlera.CrawleraMiddleware': 610
     },
     'CRAWLERA_ENABLED': True,
-    'CRAWLERA_APIKEY': '{{cookiecutter.crawlera_api_key}}'
+    'CRAWLERA_APIKEY': '{{cookiecutter.crawlera_api_key}}',
     {% endif %}
+    'FEED_FORMAT': "{{cookiecutter.output_file_type}}",
+    'FEED_URI': "{{cookiecutter.output_file_name}}.{{cookiecutter.output_file_type}}"
+
 })
 
 process.crawl({{cookiecutter.script_name | title}}Spider)
